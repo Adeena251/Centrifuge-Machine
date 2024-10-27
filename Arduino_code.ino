8cecb486ef0b7061428bff013e0fc7b6a6670c56
Adeena251 <bssadeenazafar@gmail.com>
2024-10-27 19:51:21 +0500

    Update Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -1,3 +1,5 @@
+**Tachometer Code**
+  
 #include<LiquidCrystal.h> LiquidCrystal lcd(12,11,6,5,4,3); float value=0; float rev=0; int
 rpm; int oldtime=0; int time;
 void isr() //interrupt service routine
